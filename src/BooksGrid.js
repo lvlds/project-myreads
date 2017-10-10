053a89829d7c7c318fdc0f69eaedbfd03ada1568
Luis Valdes <luischivaldes@gmail.com>
2017-10-10 19:17:35 -0300

    Simplify component
    Signed-off-by: Luis Valdes <luischivaldes@gmail.com>

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -1,23 +1,20 @@
 import React from 'react'
-
 import Book from './Book'
 
-class BooksGrid extends React.Component {
-  render(){
-    return (
-      <ol className="books-grid">
-        {
-          this.props.books.map((book, index) => {
-            return (
-              <li key={index} >
-                <Book book={book}
-                      updateBook={this.props.updateBook} />
-              </li>)
-          })
-        }
-      </ol>
-    )
-  }
+const BooksGrid = props => {
+  return (
+    <ol className="books-grid">
+      {
+        props.books.map((book, index) => {
+          return (
+            <li key={index} >
+              <Book book={book}
+                    updateBook={props.updateBook} />
+            </li>)
+        })
+      }
+    </ol>
+  )
 }
 
-export default BooksGrid;
+export default BooksGrid
